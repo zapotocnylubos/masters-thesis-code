807215ca41e3e47a8cb53020fd1cb8c4d610f79d
Lubos Zapotocny <zapotocnylubos@gmail.com>
2025-03-05 19:41:34 +0100

    AVL - Method intWrapper,max comes from the Scala standard library and is currently not supported.

---
+++ AVLTree.scala
@@ -0,0 +1,92 @@
+import stainless.lang._
+import stainless.collection._
+import stainless.annotation._
+
+sealed abstract class AVLTree {
+  def content: Set[Int] = this match {
+    case Leaf() => Set.empty
+    case Node(v, l, r, _) => l.content ++ Set(v) ++ r.content
+  }
+
+  def height: Int = this match {
+    case Leaf() => 0
+    case Node(_, _, _, h) => h
+  }
+
+  def balanceFactor: Int = this match {
+    case Leaf() => 0
+    case Node(_, l, r, _) => l.height - r.height
+  }
+
+  def isAVLTree: Boolean = this match {
+    case Leaf() => true
+    case Node(v, l, r, h) => {
+      l.isAVLTree &&
+        r.isAVLTree &&
+        forall((x: Int) => l.content.contains(x) ==> x < v) &&
+        forall((x: Int) => r.content.contains(x) ==> x > v) &&
+        balanceFactor >= -1 &&
+        balanceFactor <= 1 &&
+        h == 1 + (l.height max r.height)
+    }
+  }
+
+  def contains(x: Int): Boolean = {
+    require(isAVLTree)
+    this match {
+      case Leaf() => false
+      case Node(v, l, r, _) => {
+        if (x == v) true
+        else if (x < v) l.contains(x)
+        else r.contains(x)
+      }
+    }
+  }.ensuring(res => res == content.contains(x))
+}
+
+case class Leaf() extends AVLTree
+
+case class Node
+(
+  value: Int,
+  left: AVLTree,
+  right: AVLTree,
+  override val height: Int
+) extends AVLTree {
+  def min: Int = {
+    require(isAVLTree)
+    this match {
+      case Node(v, Leaf(), _, _) => v
+      case Node(_, l: Node, _, _) => l.min
+    }
+  }.ensuring(res =>
+    content.contains(res) &&
+      forall((x: Int) => (content.contains(x) && x != res) ==> x > res)
+  )
+
+  def max: Int = {
+    require(isAVLTree)
+    this match {
+      case Node(v, _, Leaf(), _) => v
+      case Node(_, _, r: Node, _) => r.max
+    }
+  }.ensuring(res =>
+    contains(res) &&
+      forall((x: Int) => (contains(x) && x != res) ==> x < res)
+  )
+
+//  def rotateLeft: AVLTree = {
+//    require(left.isAVLTree && right.isAVLTree)
+//    require(balanceFactor == 2 && right.balanceFactor == 1)
+//
+//    right match {
+//      case Node(v, rl, rr, _) =>
+//        Node(
+//          v,
+//          Node(value, left, rl, 1 + (left.height max rl.height)),
+//          rr,
+//          1 + (value max rr.height)
+//        )
+//    }
+//  }.ensuring(res => res.isAVLTree && res.content == content && res.height == height)
+}
