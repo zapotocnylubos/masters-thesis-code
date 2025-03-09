abd895688c2dda2b81f48e7832c54473ce3087d2
Lubos Zapotocny <zapotocnylubos@gmail.com>
2025-03-09 15:50:37 +0100

    cleanup after proving left rotation (cleaning testing lemmas with .holds)

--- AVLTree.scala
+++ AVLTree.scala
@@ -117,42 +117,16 @@ case class Node
       forall((x: Int) => (contains(x) && x != res) ==> x < res)
   )
 
-  def t(): Boolean = {
-    require(isAVLTree)
-    left.height < height && right.height < height
-  }.holds
-
-  def t2(): Boolean = {
-    require(isAVLTree)
-    left.isInstanceOf[Node] ==> left.asInstanceOf[Node].left.height < height - 1
-  }.holds
-
   def rotateLeft: Node = {
     require(hasBinarySearchTreeStructure)
     require(hasAVLTreeStructure)
+    require(left.isAVLTree && right.isAVLTree)
     require(balanceFactor == 2 && right.balanceFactor == 1)
-    require(left.isAVLTree)
-    require(right.isAVLTree)
-    require(right.isInstanceOf[Node])
-    // this helps?
-//    require(height < Int.MaxValue)
-//    require(height < Int.MaxValue - 10)
-
-
-    //    node* leftRotation(node* current) {
-    //        node* new_node = current->right;
-    //        current->right = new_node->left;
-    //        new_node->left = current;
-    //        current->height = 1 + max(height(current->left), height(current->right));
-    //        new_node->height = 1 + max(height(new_node->left), height(new_node->right));
-    //        return new_node;
-    //    }
-
 
     right match {
-      case Node(rv, rl, rr, _) =>
+      case Node(v, rl, rr, _) =>
         Node(
-          rv,
+          v,
           Node(value, left, rl, 1 + int_max(left.height, rl.height)),
           rr,
           1 + int_max(
@@ -161,50 +135,5 @@ case class Node
           )
         )
     }
-  }.ensuring(res => res.content == content &&
-    res.right.isAVLTree &&
-    res.left.isInstanceOf[Node] ==> (res.left.asInstanceOf[Node].value == old(this).value) &&
-    res.left.isInstanceOf[Node] ==> (res.left.asInstanceOf[Node].left == old(this).left) &&
-    res.left.isInstanceOf[Node] ==> (res.left.asInstanceOf[Node].right == old(this).right.asInstanceOf[Node].left) &&
-    res.left.hasBinarySearchTreeStructure &&
-    res.left.hasAVLTreeStructure &&
-    res.right.hasBinarySearchTreeStructure &&
-    res.right.hasAVLTreeStructure &&
-    res.right.isBalanced &&
-    res.left.balanceFactor >= -1 &&
-    res.left.balanceFactor <= 3
-    && res.balanceFactor <= 5 // knowledge probing -> <= 30, 15, 10, 6, 5 -> OK, <= 4 -> NOK
-    // Knowledge probing about left rotation subtrees
-    && ((res.left.isInstanceOf[Node] && res.left.asInstanceOf[Node].left.isInstanceOf[Node])
-        ==> res.left.asInstanceOf[Node].left.asInstanceOf[Node].isAVLTree)
-    && ((res.left.isInstanceOf[Node] && res.left.asInstanceOf[Node].right.isInstanceOf[Node])
-        ==> res.left.asInstanceOf[Node].right.asInstanceOf[Node].isAVLTree)
-    // Knowledge probing about right heigh of rotated node
-    && res.left.isInstanceOf[Node] ==> (res.left.asInstanceOf[Node].height == 1 + (res.left.asInstanceOf[Node].left.height < res.left.asInstanceOf[Node].right.height match {
-      case true => res.left.asInstanceOf[Node].right.height
-      case false => res.left.asInstanceOf[Node].left.height
-    }))
-    && res.right.isInstanceOf[Node] ==> (res.right.asInstanceOf[Node].height == 1 + (res.right.asInstanceOf[Node].left.height < res.right.asInstanceOf[Node].right.height match {
-      case true => res.right.asInstanceOf[Node].right.height
-      case false => res.right.asInstanceOf[Node].left.height
-    }))
-    //
-    && res.left.isInstanceOf[Node] ==> (res.left.asInstanceOf[Node].height == 1 + (res.left.asInstanceOf[Node].left.height < res.left.asInstanceOf[Node].right.height match {
-      case true => res.left.asInstanceOf[Node].right.height
-      case false => res.left.asInstanceOf[Node].left.height
-    }))
-    // knowledge probing about keeping height
-    && ((res.left.isInstanceOf[Node] && res.left.asInstanceOf[Node].left.isInstanceOf[Node])
-      ==> (res.left.asInstanceOf[Node].left.asInstanceOf[Node].height == old(this).left.height))
-    && ((res.left.isInstanceOf[Node] && res.left.asInstanceOf[Node].right.isInstanceOf[Node])
-      ==> (res.left.asInstanceOf[Node].right.asInstanceOf[Node].height == old(this).right.asInstanceOf[Node].left.height))
-    && res.left.isInstanceOf[Node] ==> (res.left.balanceFactor == (res.left.asInstanceOf[Node].left.height - res.left.asInstanceOf[Node].right.height))
-    && res.left.isInstanceOf[Node] ==> ((res.left.asInstanceOf[Node].left.height - res.left.asInstanceOf[Node].right.height) <= 2)
-//    && res.left.isInstanceOf[Node] ==> ((res.left.asInstanceOf[Node].left.height - res.left.asInstanceOf[Node].right.height) <= 2)
-//    && res.left.balanceFactor <= 2
-//    res.left.balanceFactor <= 1
-    && res.left.isBalanced
-    && res.isAVLTree
-  )
-   //&& res.isAVLTree)
+  }.ensuring(res => res.content == content && res.isAVLTree)
 }
