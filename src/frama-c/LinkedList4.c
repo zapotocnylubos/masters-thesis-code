6a47f38509f019808b7fc60b6f96b29486ba5a95
Lubos Zapotocny <zapotocnylubos@gmail.com>
2025-03-19 10:48:01 +0100

    F-c LL prepend axiom works

--- src/frama-c/LinkedList4.c
+++ src/frama-c/LinkedList4.c
@@ -1,77 +1,113 @@
 #include <stdlib.h>
 
-struct node {
+typedef struct list {
+    struct list *next;
     int value;
-    struct node *next;
-};
+} List;
 
 /*@
-    logic integer length(struct node *head) =
-      head == \null ? 0 : 1 + length(head->next);
-*/
-
-/*@
-    inductive linked_list{L}(struct node *head) {
-      case empty{L}:
-        linked_list(\null);
-
-      case node{L}:
-        \forall struct node *n;
-          \valid(n) &&
-              linked_list(n->next) ==> linked_list(n);
-    }
-*/
-
-
-/*@
-    inductive reachable{L}(struct node *root, struct node *node) {
+    inductive reachable{L}(List *root, List *node) {
         case root_reachable{L}:
-            \forall struct node *root;
+            \forall List *root;
                 reachable(root, root);
 
         case next_reachable{L}:
-            \forall struct node *root, *node;
+            \forall List *root, *node;
                 \valid(root) &&
                     reachable(root->next, node) ==>
                         reachable(root, node);
     }
 
-    predicate finite(struct node *root) = reachable(root, \null);
+    predicate finite{L}(List *root) = reachable(root, \null);
 */
 
-/*
-    lemma prepend_lemma:
-        \forall struct node *head, *new_node;
-            linked_list(head) &&
-            !reachable(head, new_node) &&
-            new_node->next == head ==>
-                linked_list(new_node);
- */
+/*@ axiomatic Length {
+    logic integer length(List *head);
+
+    axiom length_null: length(\null) == 0;
+
+    axiom length_list:
+        \forall List *head;
+            \valid(head) && finite(head) ==>
+                length(head) == 1 + length(head->next);
+
+    axiom length_nonnegative:
+        \forall List *l;
+            finite(l) ==> length(l) >= 0;
+}
+*/
 
 /*@
-    requires linked_list(head);
+    requires finite(head);
 
+    terminates \true;
+    assigns \nothing;
+ */
+void list_iteration(List *head) {
+    List *p = head;
+
+    /*@
+        loop invariant finite(p);
+        loop assigns p;
+        loop variant length(p);
+     */
+    while (p != NULL) {
+        p = p->next;
+    }
+}
+
+/*@
+    requires finite(head);
+
+    assigns \nothing;
+
+    ensures \result == length(head);
+ */
+int list_length(List *head) {
+    int len = 0;
+    List *p = head;
+
+    /*@
+        loop invariant finite(p);
+        loop invariant len >= 0;
+        loop invariant len + length(p) == length(head);
+
+        loop assigns len, p;
+        loop variant length(p);
+     */
+    while (p != NULL) {
+        len++;
+        p = p->next;
+    }
+
+    return len;
+}
+
+/*@ axiomatic PrependLemma {
+    axiom prepend_keeps_finite{L1, L2}:
+        \forall List *head, *new_node;
+            \at(new_node->next, L2) == head &&
+            finite{L1}(head) ==>
+                finite{L2}(new_node);
+}
+*/
+
+/*@ requires finite(head);
     requires \valid(new_node);
-    requires \separated(new_node, { node | struct node *node; reachable(head, node) });
 
     assigns new_node->next;
  */
-struct node *prepend(
-        struct node *const head,
-        struct node *new_node
-) {
-    //@ assert linked_list(head);
+List *prepend(List *head, List *new_node) {
+    //@ assert head != \null ==> reachable(head, head->next);
+    //@ assert finite(head);
 
     new_node->next = head;
 
-    //@ assert linked_list(head);
-    //@ assert linked_list(new_node);
+    //@ assert head != \null ==> reachable(head, head->next);
+    //@ assert finite(head);
+    //@ assert finite(new_node);
 
     //@ assert \false;
 
     return new_node;
-}
-
-void test_contradiction() {
-    //@ assert \false;
 }
