b919cb5ac3c05589741ff21c0955dfe304d61d30
Lubos Zapotocny <zapotocnylubos@gmail.com>
2025-03-21 16:12:23 +0100

    F-c not even local variable helps, the memory model still does no know about separation or aliasing??

--- src/frama-c/prepend-ll-problem.c
+++ src/frama-c/prepend-ll-problem.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct list {
     int value;
@@ -61,8 +63,7 @@ typedef struct list {
 
     assigns \nothing;
 */
-void test_reachable(List *head)
-{
+void test_reachable(List *head) {
     //@ assert reachable(head, head);
     //@ assert reachable(head, head->next);
     // assert !reachable(head->next, head);
@@ -75,19 +76,34 @@ void test_reachable(List *head)
     requires finite(head);
 
     requires \valid(new_node);
-    requires new_node != head;
     requires \separated(new_node, { node | List *node; reachable(head, node) });
 
     assigns \nothing;
  */
-List *prepend(List *head, List *new_node) {
+List prepend(List *head, List *new_node) {
     //@ assert finite(head);
-
-    new_node->next = NULL;
+    List new_head;
+    new_head.value = new_node->value;
+    new_head.next = head;
 
     //@ assert reachable(\at(head, Pre), \null);
 
     //@ assert finite(head);
-    //@ assert finite(new_node);
-    return new_node;
+    //@ assert finite(&new_head);
+    return new_head;
+}
+
+int main() {
+    List *head;
+    //@ assert finite(head);
+
+    List *new_node = (List *) malloc(sizeof(List));
+    if (!new_node) {
+        return 1;
+    }
+    new_node->value = 10;
+
+    List new_head = prepend(head, new_node);
+
+    return 0;
 }
