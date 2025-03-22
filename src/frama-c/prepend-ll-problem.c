c69aaaa390255110fb4dd425b9af2aa1d9e81809
Lubos Zapotocny <zapotocnylubos@gmail.com>
2025-03-22 17:23:41 +0100

    F-c simple variables do not modify the Malloc_0 memory

--- src/frama-c/prepend-ll-problem.c
+++ src/frama-c/prepend-ll-problem.c
@@ -7,7 +7,7 @@ typedef struct list {
     struct list *next;
 } List;
 
-/*@
+/*
     inductive linked{L}(List *root) {
         case root_linked{L}:
             \forall List *root;
@@ -29,7 +29,7 @@ typedef struct list {
 
         case next_reachable{L}:
             \forall List *root, *node;
-                \valid(root) &&
+                \valid_read(root) &&
                     reachable(root->next, node) ==>
                         reachable(root, node);
     }
@@ -47,7 +47,7 @@ typedef struct list {
 
     axiom length_list{L}:
         \forall List *head;
-            \valid(head) && finite(head) ==>
+            \valid_read(head) && finite(head) ==>
                 length(head) == 1 + length(head->next);
 
     axiom length_nonnegative{L}:
@@ -57,7 +57,7 @@ typedef struct list {
 */
 
 /*@
-    requires \valid(head);
+    requires \valid_read(head);
     requires finite(head);
     requires length(head) == 1;
 
@@ -72,38 +72,39 @@ void test_reachable(List *head) {
 
 
 /*@
-    requires \valid(head);
+    requires \valid_read(head);
     requires finite(head);
 
-    requires \valid(new_node);
+    requires \valid_read(new_node);
     requires \separated(new_node, { node | List *node; reachable(head, node) });
 
     assigns \nothing;
  */
-List prepend(List *head, List *new_node) {
+void prepend(List *head, List *new_node) {
+    int x;
+    //List new_head;
     //@ assert finite(head);
-    List new_head;
-    new_head.value = new_node->value;
-    new_head.next = head;
+    //new_head.value = new_node->value;
+    //new_head.next = head;
 
     //@ assert reachable(\at(head, Pre), \null);
 
     //@ assert finite(head);
-    //@ assert finite(&new_head);
-    return new_head;
+    // assert finite(&new_head);
+    return;
 }
 
 int main() {
-    List *head;
-    //@ assert finite(head);
-
-    List *new_node = (List *) malloc(sizeof(List));
-    if (!new_node) {
-        return 1;
-    }
-    new_node->value = 10;
-
-    List new_head = prepend(head, new_node);
-
-    return 0;
+//    List *head;
+//    //@ assert finite(head);
+//
+//    List *new_node = (List *) malloc(sizeof(List));
+//    if (!new_node) {
+//        return 1;
+//    }
+//    new_node->value = 10;
+//
+//    List new_head = prepend(head, new_node);
+//
+//    return 0;
 }
