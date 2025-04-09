1a8ae521d247a03305ca08bd00250c4f868ec95f
Lubos Zapotocny <zapotocnylubos@gmail.com>
2025-04-09 22:56:22 +0200

    ghost param in function -- contiki wrong

---
+++ src/frama-c/ghost-param.c
@@ -0,0 +1,17 @@
+/*@ ghost int g_function (int x){
+ @   return x + 42;
+ @ }
+ @*/
+
+//@ ghost int glob;
+
+void function(int x) /*@ ghost (int parameter ) */ {
+    //@ ghost int save_x = x;
+    x++;
+    //@ ghost glob = g_function(save_x + parameter);
+    //@ assert glob == save_x + parameter + 42;
+}
+
+void caller(void) {
+    function(3) /*@ ghost (4) */ ;
+}
