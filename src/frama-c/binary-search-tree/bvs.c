b245ce66943d0fa74aae75755026aa89e4debb04
Lubos Zapotocny <zapotocnylubos@gmail.com>
2025-05-05 23:36:20 +0200

    the one admit for ll prepend

--- src/frama-c/binary-search-tree/bvs.c
+++ src/frama-c/binary-search-tree/bvs.c
@@ -12,93 +12,242 @@ typedef struct binary_search_tree {
         case null_hasBinarySearchTreeStructure:
             hasBinarySearchTreeStructure(\null);
 
+        case leaf_hasBinarySearchTreeStructure:
+            \forall BinarySearchTree *node;
+                \valid(node)
+                && node->left == \null
+                && node->right == \null
+                ==>
+                    hasBinarySearchTreeStructure(node);
+
+        case node_hasBinarySearchTreeStructure:
+            \forall BinarySearchTree *node, integer value;
+                \valid(node)
+                && hasBinarySearchTreeStructure(node->left)
+                && hasBinarySearchTreeStructure(node->right)
+                && node->value  == value
+                && (node->left  == \null || node->left->value < value)
+                && (node->right == \null || node->right->value > value)
+                ==>
+                    hasBinarySearchTreeStructure(node);
     }
 */
+
+/*
+    logic set<BinarySearchTree*> footprint(BinarySearchTree *root) =
+        root == \null ? \empty :
+            \union({ root }, footprint(root->left), footprint(root->right));
+
+    logic set<integer> content(BinarySearchTree *root) =
+        root == \null ? \empty :
+            \union({ root->value }, content(root->left), content(root->right));
+ */
+
+/*@
+    axiomatic BinaryTreeFootprint {
+        logic set<BinarySearchTree*> footprint(BinarySearchTree *root);
+
+        axiom footprint_null: footprint(\null) == \empty;
+
+        axiom footprint_node:
+            \forall BinarySearchTree *node;
+                \valid(node)
+                ==>
+                    footprint(node) == \union({ node }, footprint(node->left), footprint(node->right));
+    }
+ */
+
+/*@
+    predicate reachable(BinarySearchTree *root, BinarySearchTree *node) =
+        root == node
+        || (
+            \valid(root)
+            && (
+                reachable(root->left, node)
+                || reachable(root->right, node)
+            )
+        );
+ */
+
+/*@
+    predicate acyclic(BinarySearchTree *root) =
+        root == \null
+        || (
+            \valid(root)
+            && acyclic(root->left)
+            && acyclic(root->right)
+            && !reachable(root->left, root)
+            && !reachable(root->right, root)
+        );
+ */
+
+/*@
+    predicate acyclic_mem(BinarySearchTree *root) =
+        root == \null
+        || (
+            \valid(root)
+            && acyclic_mem(root->left)
+            && acyclic_mem(root->right)
+            && \inter(footprint(root->left), footprint(root->right)) == \empty
+            && \inter(footprint(root->left), footprint(root)) == \empty
+            && \inter(footprint(root->right), footprint(root)) == \empty
+        );
+ */
 
 /*@
     logic integer height(BinarySearchTree *node) = (node == \null) ? 0 :
         \max(1 + height(node->left), 1 + height(node->right));
  */
 
-/*@
-    axiomatic HeightProperties {
-      axiom height_null_zero:
-        height(\null) == 0;
+/* axiomatic Length {
+    logic integer height(BinarySearchTree *node);
 
+    axiom height_null: height(\null) == 0;
+
+    axiom height_node:
+        \forall BinarySearchTree *node;
+            \valid(node)
+            && hasBinarySearchTreeStructure(node) ==>
+                height(node) == 1 + \max(height(node->left), height(node->right));
+
+    axiom height_nonnegative:
+        \forall BinarySearchTree *node;
+            hasBinarySearchTreeStructure(node) ==>
+                0 <= height(node);
+}
+*/
+
+/*
+    axiomatic HeightProperties {
       axiom height_nonnegative:
-        \forall BinarySearchTree *l;
-            hasBinarySearchTreeStructure(l) ==>
-                0 <= height(l);
+        \forall BinarySearchTree *node;
+            hasBinarySearchTreeStructure(node) ==>
+                0 <= height(node);
 
       axiom height_strict_higher_than_left:
         \forall BinarySearchTree *node;
             \valid(node)
-            && hasBinarySearchTreeStructure(node) ==>
-               height(node) > height(node->left);
+            && hasBinarySearchTreeStructure(node)
+            ==>
+                height(node->left) < height(node);
 
       axiom height_strict_higher_than_right:
         \forall BinarySearchTree *node;
             \valid(node)
-            && hasBinarySearchTreeStructure(node) ==>
-               height(node) > height(node->right);
+            && hasBinarySearchTreeStructure(node)
+            ==>
+                height(node->right) < height(node);
     }
 */
 
 /*@
     requires hasBinarySearchTreeStructure(root);
+    requires acyclic(root);
+    requires acyclic_mem(root);
+
     ensures hasBinarySearchTreeStructure(\result);
+    ensures acyclic(\result);
 */
 BinarySearchTree *insert(BinarySearchTree *root, int value) {
-    BinarySearchTree *result = root;
-
-    // check if the tree is empty
-    // if the tree is empty, create a single node
-    // and return it
     if (root == NULL) {
-        BinarySearchTree *result = (BinarySearchTree *)malloc(sizeof(BinarySearchTree));
-        if (result != NULL) {
-            result->left = NULL;
-            result->right = NULL;
-            result->value = value;
-        }
-    } else {
-        // Otherwise, recur down the tree
-        BinarySearchTree *currentRoot = root;
-        BinarySearchTree *parent = NULL;
-
-        /*@
-             loop assigns currentRoot, parent;
-             loop variant height(currentRoot);
-         */
-        while (currentRoot != NULL) {
-            parent = currentRoot;
-            if (value < currentRoot->value) {
-                currentRoot = currentRoot->left;
-            } else if (value > currentRoot->value) {
-                currentRoot = currentRoot->right;
-            } else {
-                // Value already exists in the tree
-                return root;
-            }
-        }
-
-        BinarySearchTree *new_node = (BinarySearchTree *)malloc(sizeof(BinarySearchTree));
+        BinarySearchTree *new_node = (BinarySearchTree *) malloc(sizeof(BinarySearchTree));
         if (new_node == NULL) {
             return NULL; // Memory allocation failed
         }
-        // mini-axiomatic hint for provers
-        // admit \valid(new_node);
+        //@ admit \valid(new_node);
+        //@ assert !reachable(root, new_node);
 
         new_node->left = NULL;
         new_node->right = NULL;
         new_node->value = value;
 
-        if (value < parent->value) {
-            parent->left = new_node;
-        } else {
-            parent->right = new_node;
-        }
+        //@ assert hasBinarySearchTreeStructure(new_node);
+        return new_node;
     }
 
-    return result;
+//    BinarySearchTree *parent = NULL;
+//    BinarySearchTree *current = root;
+
+//    /*@
+//        loop invariant hasBinarySearchTreeStructure(root);
+//        loop invariant hasBinarySearchTreeStructure(current);
+//        loop invariant hasBinarySearchTreeStructure(parent);
+//        loop assigns parent, current;
+//        loop variant height(current);
+//     */
+//    while (current != NULL) {
+//        parent = current;
+//        if (value < current->value) {
+//            current = current->left;
+//        } else if (value > current->value) {
+//            current = current->right;
+//        } else {
+//            // Value already exists in the tree
+//            //@ assert hasBinarySearchTreeStructure(root);
+//            return root;
+//        }
+//    }
+
+    //@ assert hasBinarySearchTreeStructure(root);
+    root->left = NULL;
+    //root->right = NULL;
+
+    //@ assert \valid(root);
+    //@ assert hasBinarySearchTreeStructure(root->left);
+    //@ assert hasBinarySearchTreeStructure(root->right);
+    //@ assert (root->left  == \null || root->left->value < root->value);
+    //@ assert (root->right == \null || root->right->value > root->value);
+
+    // assert \valid(root);
+    // assert root->left  == \null;
+    // assert root->right == \null;
+    //@ assert hasBinarySearchTreeStructure(root);
+
+    BinarySearchTree *new_node = (BinarySearchTree *) malloc(sizeof(BinarySearchTree));
+    if (new_node == NULL) {
+        return NULL; // Memory allocation failed
+    }
+    // admit \valid(new_node);
+    // admit !reachable(root, new_node);
+    // assert !reachable(root, new_node);
+
+
+    /*
+        admit \separated(new_node, {
+            node | BinarySearchTree *node; reachable(root, node)
+        });
+     */
+    /*
+        admit \separated(new_node->left, {
+            node | BinarySearchTree *node; reachable(root, node)
+        });
+     */
+
+    // assert \separated(new_node, root);
+    // assert hasBinarySearchTreeStructure(root);
+    new_node->left = NULL;
+
+    // assert \separated(new_node, root);
+    //@ assert hasBinarySearchTreeStructure(root);
+    new_node->right = NULL;
+
+    //@ assert hasBinarySearchTreeStructure(new_node);
+    // assert hasBinarySearchTreeStructure(root);
+    new_node->value = value;
+
+    //@ assert hasBinarySearchTreeStructure(new_node);
+    // assert hasBinarySearchTreeStructure(parent);
+
+//    if (value < parent->value) {
+//        parent->left = new_node;
+//    } else {
+//        parent->right = new_node;
+//    }
+
+    // assert hasBinarySearchTreeStructure(parent);
+    //@ assert hasBinarySearchTreeStructure(root);
+
+    return root;
 }
+
+
---
+++ src/frama-c/linked-list/ll.c
@@ -0,0 +1,203 @@
+#include <stdlib.h>
+
+typedef struct list {
+    struct list *next;
+    int value;
+} List;
+
+/*@
+    inductive reachable(List *root, List *node) {
+        case root_reachable:
+            \forall List *root;
+                reachable(root, root);
+
+        case next_reachable:
+            \forall List *root, *node;
+                \valid(root) &&
+                    reachable(root->next, node) ==>
+                        reachable(root, node);
+    }
+*/
+
+/*@
+    inductive finite(List *root) {
+        case null_finite:
+            finite(\null);
+
+        case node_finite:
+            \forall List *node;
+                \valid(node)
+                && finite(node->next)
+                ==>
+                    finite(node);
+    }
+ */
+
+/*
+    predicate finite(List *root) = reachable(root, \null);
+*/
+
+/*@
+    inductive SameLists(List *root1, List *root2) {
+        case null_same:
+            \forall List *root1, *root2;
+                root1 == \null
+                && root2 == \null
+                ==>
+                    SameLists(root1, root2);
+
+        case node_same:
+            \forall List *root1, *root2;
+                \valid(root1)
+                && \valid(root2)
+                && root1 == root2
+                && SameLists(root1->next, root2->next)
+                ==>
+                    SameLists(root1, root2);
+    }
+ */
+
+/*@
+    axiomatic finite_lemma {
+        axiom finite_chain:
+            \forall List *node, *children;
+                \valid(node)
+                && finite(node)
+                && reachable(node, children)
+                ==>
+                    finite(children);
+    }
+ */
+
+/*@
+    axiomatic Length {
+        logic integer length(List *head);
+
+        axiom length_null:
+            length(\null) == 0;
+
+        axiom length_list:
+            \forall List *head;
+                \valid_read(head)
+                && finite(head) ==>
+                    length(head) == 1 + length(head->next);
+
+        axiom length_nonnegative:
+            \forall List *l;
+                finite(l)
+                  ==> length(l) >= 0;
+    }
+*/
+
+/*@
+    logic set<List*> footprint(List *head) = \union(head, footprint(head->next));
+ */
+
+/*
+    axiomatic LinkedListMemoryFootprint {
+        logic set<List*> footprint(List *root);
+
+        axiom footprint_null: footprint(\null) == \empty;
+
+        axiom footprint_node:
+            \forall List *node;
+                \valid(node)
+                && finite(node)
+                ==>
+                    (footprint(node) == \union({ node }, footprint(node->next)));
+    }
+*/
+
+
+/*@
+    axiomatic separated_lemma {
+        lemma a:
+            \forall int *a, *b, c;
+                \valid(a) && \valid(b)
+                && \separated(a, b)
+                && *a == c
+                && *b != c
+                  ==> a != b;
+    }
+ */
+
+/*@
+    requires \valid(head);
+    requires finite(head);
+    requires length(head) < 100;
+
+    ensures finite(\result);
+ */
+List *prepend(List *head, int value) {
+    List *new_node = (List *)malloc(sizeof(List));
+    if (!new_node) {
+        return NULL; // Handle memory allocation failure
+    }
+    //@ admit \valid(new_node);
+
+    //@ admit \forall List *node; reachable(head, node) ==> \separated(new_node, node);
+    //@ admit \forall List *node; reachable(head, node) ==> new_node != node;
+    // admit \forall List *node; reachable(head, node) ==> new_node->next != node;
+
+    // admit !(new_node \in footprint(head));
+    // admit \subset(\inter({new_node}, footprint(head)), \inter({new_node}, footprint(head)));
+    // admit \separated(new_node, footprint(head));
+    // admit new_node != head;
+
+    // assert finite(head);
+//    new_node->value = value;
+    // assert finite(head);
+    //new_node->next = NULL;
+    // admit \separated(new_node, footprint(head));
+    // assert finite(new_node);
+    // assert finite(head);
+
+    // assert finite(head);
+    // assert \forall List *node; reachable(head, node) ==> finite(node);
+
+    //@ assert head != \null ==> footprint(head) == \union({ head }, footprint(head->next));
+    // assert SameLists(head, head);
+
+    L0:
+    new_node->next = head;
+
+    //@ admit head != \null ==> footprint{L0}(head) == \union({ head }, footprint(head->next));
+    //@ assert head != \null ==> footprint(head) == \union({ head }, footprint(head->next));
+
+    // assert head == \at(head, L0);
+    // admit \forall List *node; reachable{L0}(head, node) <==> reachable(head, node);
+
+    // assert SameLists(head, \at(head, L0));
+
+
+    // assert \forall List *node; reachable(head, node) ==> node == \at(node, L0);
+    // assert finite(head);
+
+    // ---- this is the one
+    //@ admit \forall List *node; reachable{L0}(head, node) ==> finite(node);
+    // ---- this is the one
+
+    // assert \forall List *node; reachable(head, node) ==> finite(node);
+
+    // admit \forall List *node; reachable(head, node) ==> \separated(new_node, node);
+    // admit \forall List *node; reachable(head, node) ==> new_node != node;
+
+    // admit \forall List *node; reachable(head, node) ==> \separated(new_node, node);
+    // admit \forall List *node; reachable{L0}(head, node) ==> \separated(new_node->next, node);
+    // admit \forall List *node; reachable(head, node) ==> new_node != node;
+    // admit \forall List *node; reachable(head->next, node) ==> new_node->next != node;
+
+    // admit !(new_node \in footprint(head));
+    // admit \subset(\inter({new_node}, footprint(head)), \inter({new_node}, footprint(head)));
+    // admit \separated(new_node, footprint(head));
+    // admit new_node != head;
+    // admit finite(head->next);
+
+    // assert finite{L0}(head) && (\forall List *node; reachable(head, node) ==> \separated(new_node, node)) ==> finite(head);
+    //@ assert finite(head);
+    // assert finite(new_node);
+
+    //@ assert \false;
+
+    return new_node;
+}
